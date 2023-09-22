4ea68fd35e6e2bdf7066fd822cc6fb3dbf0a7d1f
astonlzr <astonlazar2k2@gmail.com>
2023-09-22 10:47:44 +0530

    Airbnb

---
+++ script.js
@@ -0,0 +1,9 @@
+function signuptab() {
+  var x = document.getElementById("profile-box");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
